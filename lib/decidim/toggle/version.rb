246e6ddcc8c4dda80c47d2286f28927182fad657
Hadrien Froger <hadrien@octree.ch>
2026-06-30 16:16:03 +0100

    release(0.1.3): Rubygem release with 0.29..0.31 support

--- lib/decidim/toggle/version.rb
+++ lib/decidim/toggle/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-toggle version.
   module Toggle
     def self.version
-      "0.1.2" # DO NOT UPDATE MANUALLY
+      "0.1.3" # DO NOT UPDATE MANUALLY
     end
 
     def self.decidim_version
