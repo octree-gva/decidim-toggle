9c2231277f60fb7d2b0dd5cc13bca09e536d6526
Hadrien Froger <hadrien@octree.ch>
2026-08-10 16:23:21 +0100

    fix: 0.29 compat

--- config/assets.rb
+++ config/assets.rb
@@ -2,7 +2,10 @@
 
 base_path = File.expand_path("..", __dir__)
 
-Decidim::Shakapacker.register_path("#{base_path}/app/packs")
-Decidim::Shakapacker.register_entrypoints(
+# Decidim 0.29 uses Webpacker; 0.32+ uses Shakapacker.
+registry = defined?(Decidim::Shakapacker) ? Decidim::Shakapacker : Decidim::Webpacker
+
+registry.register_path("#{base_path}/app/packs")
+registry.register_entrypoints(
   decidim_toggle: "#{base_path}/app/packs/entrypoints/decidim_toggle.js"
 )
